e0cee07c9020ac729f78e89858d57f098c8a9505
sernuxamed <>
2026-03-19 20:03:07 +0000

    Variable calculations

--- variablen_und_typen/variable_calculations/main.py
+++ variablen_und_typen/variable_calculations/main.py
@@ -1,5 +1,6 @@
-
-
-
+item_name = "Soda"
+item_price = 6.99
+purchase_quantity = 5
+total_cost = purchase_quantity * item_price
 # Testing
 print("Total cost for", purchase_quantity, "cases of", item_name, "is $", total_cost)
