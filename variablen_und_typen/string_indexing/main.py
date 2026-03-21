e2c790c4f78db9975e42c218ec9d4c749b96b215
sernuxamed <>
2026-03-21 14:49:43 +0000

    String Indexing

--- variablen_und_typen/string_indexing/main.py
+++ variablen_und_typen/string_indexing/main.py
@@ -1,6 +1,14 @@
 grocery_item = "Grilled Chicken Salad"
 
 
+length_of_item = len(grocery_item)
+
+first_char = grocery_item[0]
+second_char = grocery_item[8]
+third_char = grocery_item[16]
+last_char1 = grocery_item[-1]
+last_char2 = grocery_item[-7]
+last_char3 = grocery_item[-15]
 
 
 # Testing
